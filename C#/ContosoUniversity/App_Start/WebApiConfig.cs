e58d537c90f1509499d899fc25da1e2a668eaa3b
Jimmy Bogard <jimmy.bogard@gmail.com>
2015-07-01 11:17:17 -0500

    Example

---
+++ C#/ContosoUniversity/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace ContosoUniversity
+{
+    using System.Web.Http;
+    using CollectionJson.Client;
+    using Newtonsoft.Json.Serialization;
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Formatters.Insert(0, new CollectionJsonFormatter());
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new {id = RouteParameter.Optional});
+        }
+    }
+}
---
+++ C#/ContosoUniversity/Helpers/UrlHelperExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace ContosoUniversity.Helpers
+{
+    using System;
+    using System.Linq.Expressions;
+    using System.Web.Http;
+    using System.Web.Http.Routing;
+    using Ploeh.Hyprlinkr;
+
+    public static class UrlHelperExtensions
+    {
+        public static Uri Link<T, TResult>(this UrlHelper helper, Expression<Func<T, TResult>> expression)
+            where T : ApiController
+        {
+            var linker = new RouteLinker(helper.Request);
+
+            return linker.GetUri(expression);
+        }
+
+        public static Uri Link<T>(this UrlHelper helper, Expression<Action<T>> expression)
+            where T : ApiController
+        {
+            var linker = new RouteLinker(helper.Request);
+
+            return linker.GetUri(expression);
+        }
+    }
+}
