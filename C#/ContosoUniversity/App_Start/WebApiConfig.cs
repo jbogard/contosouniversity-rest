08742853f8e6007567f7c81b8996a812053167f9
Jimmy Bogard <jimmy.bogard@gmail.com>
2015-11-15 21:34:48 -0600

    React version

--- C#/ContosoUniversity/App_Start/WebApiConfig.cs
+++ C#/ContosoUniversity/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿namespace ContosoUniversity
 {
     using System.Web.Http;
+    using System.Web.Http.Cors;
     using CollectionJson.Client;
     using Newtonsoft.Json.Serialization;
 
@@ -8,6 +9,9 @@
     {
         public static void Register(HttpConfiguration config)
         {
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
             config.Formatters.Insert(0, new CollectionJsonFormatter());
             config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
             config.MapHttpAttributeRoutes();
